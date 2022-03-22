46ab5114ca374e8943d28bac46a9a40236f95901
zhenshan <zhenshan@belmont.tech>
2022-03-22 16:08:34 -0500

    first commit

---
+++ LocationExtraction/main.py
@@ -0,0 +1,50 @@
+from geopy.geocoders import Nominatim
+import spacy
+  
+nlp = spacy.load('en_core_web_sm')
+
+
+def address_to_coordinates(address):
+    # Geopy has different Geocoding services that you can choose from, including Google Maps, ArcGIS, AzureMaps, Bing, etc. Some of them require API keys, while others do not need.
+    # Complete list of encoder: https://geopy.readthedocs.io/en/latest/#module-geopy.geocoders
+    # Nominatim Geocoding service, which is built on top of OpenStreetMap data
+    
+    geolocator = Nominatim(user_agent="daisi_geocoder")
+
+    location = geolocator.geocode(address)
+
+    data = {
+        "address": location.address if location else None,
+        "latitude": location.latitude if location else None,
+        "longitude": location.longitude if location else None,
+    }
+
+    return data
+
+def coordinates_to_address(latitude, longitude):
+    geolocator = Nominatim(user_agent="daisi_geocoder")
+    location = geolocator.reverse(f"{latitude}, {longitude}")
+    
+    data = {
+        "address": location.address if location else None,
+        "latitude": location.latitude if location else None,
+        "longitude": location.longitude if location else None,
+    }
+    
+    return data
+
+def get_locations(text):
+    doc = nlp(text)
+    
+    locations = []
+    for ent in doc.ents:
+        if ent.label_ in ["GPE"]:
+            coordinates = address_to_coordinates(ent.text)
+            locations.append(coordinates)
+    
+    return locations
+
+
+if __name__ == "__main__": 
+    print(address_to_coordinates("Champ de Mars, Paris, France"))
+    print(coordinates_to_address(52.509669, 13.376294))
