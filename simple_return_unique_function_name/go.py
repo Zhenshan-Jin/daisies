7c50c048f7c24568f0d28af90b9ee7867e0cc8fa
zhenshan <zhenshan@belmont.tech>
2022-05-19 12:13:35 -0500

    123

---
+++ simple_return_unique_function_name/go.py
@@ -0,0 +1,11 @@
+import time
+
+def compute_go(name):
+    time.sleep(3)
+    return name
+
+def run_go(name):
+    time.sleep(3)
+    return name
+
+# add comments
---
+++ simple_return_unique_function_name/main.py
@@ -0,0 +1,11 @@
+import time
+
+def compute(name):
+    time.sleep(3)
+    return name
+
+def run(name):
+    time.sleep(3)
+    return name
+
+# add comments
