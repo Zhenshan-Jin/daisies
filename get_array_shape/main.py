b79caacfed73cc94fe4e938539aabca0cfdc54a0
zhenshan <zhenshan@belmont.tech>
2022-05-24 15:42:55 -0500

    123

--- get_array_shape/main.py
+++ get_array_shape/main.py
@@ -5,7 +5,11 @@ import pandas as pd
 def get_size(array:pd.DataFrame):
     """
     image: object from PIL.image.open
+    
+    :param array: here are some addtional comment
     """
     with open(array, "rb") as istr:
         arr = np.frombuffer(istr.read())
     return arr.size
+
+
